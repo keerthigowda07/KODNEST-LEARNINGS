8e72273af06b815b2e72b9640d8cac6152a572b6
Keerthan H P <68839937+keerthigowda07@users.noreply.github.com>
2023-08-31 12:12:50 +0530

    Delete Java Pattern Programs Practices/PatterProgramming directory

--- Java Pattern Programs Practices/PatterProgramming/src/com/kodnest/javapattern/level01/patternprogram01/Main.java
+++
@@ -1,8 +0,0 @@
-package com.kodnest.patternprogram01;
-
-public class Main {
-
-	public static void main(String[] args) {
-		System.out.print("*");
-	}
-}
--- Java Pattern Programs Practices/PatterProgramming/src/com/kodnest/javapattern/level01/patternprogram02/Main.java
+++
@@ -1,8 +0,0 @@
-package com.kodnest.patternprogram02;
-
-public class Main {
-
-	public static void main(String[] args) {
-		System.out.println("*");
-	}
-}
